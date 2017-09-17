a339bc5a0ddaf77a580dd078faef616b07c64003
crystalDf <chendong333@gmail.com>
2017-09-17 20:59:41 +0800

    Chapter 06

--- src/ch06/sec04/TypeVarianceAndWildcardsTest.java
+++ src/ch06/sec04/TypeVarianceAndWildcardsTest.java
@@ -47,4 +47,29 @@ public class TypeVarianceAndWildcardsTest {
             }
         }
     }
+
+    public static boolean hasNulls(ArrayList<?> elements) {
+
+        for (Object element : elements) {
+
+            if (element == null) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void swap(ArrayList<?> elements, int i, int j) {
+
+        swapHelper(elements, i, j);
+    }
+
+    private static <T> void swapHelper(ArrayList<T> elements, int i, int j) {
+
+        T temp = elements.get(i);
+        elements.set(i, elements.get(j));
+        elements.set(j, temp);
+    }
+
 }

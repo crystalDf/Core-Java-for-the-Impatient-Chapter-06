fb698b705f6e1db0a9a1faac9c4c5ade85dea582
crystalDf <chendong333@gmail.com>
2017-09-19 22:12:15 +0800

    Chapter 06

--- src/ch06/sec06/RestrictionsOnGenerics.java
+++ src/ch06/sec06/RestrictionsOnGenerics.java
@@ -1,5 +1,6 @@
 package ch06.sec06;
 
+import java.lang.reflect.Array;
 import java.util.function.IntFunction;
 
 public class RestrictionsOnGenerics {
@@ -7,11 +8,25 @@ public class RestrictionsOnGenerics {
     public static void main(String[] args) {
 
         String[] greetings = repeat(10, "hi", String[]::new);
+
+        greetings = repeat(10, "hi", String.class);
     }
 
-    private static <T> T[] repeat(int n, T object, IntFunction<T[]> constr) {
+    private static <T> T[] repeat(int n, T object, IntFunction<T[]> constructor) {
 
-        T[] result = constr.apply(n);
+        T[] result = constructor.apply(n);
+
+        for (int i = 0; i < n; i++) {
+
+            result[i] = object;
+        }
+
+        return result;
+    }
+
+    private static <T> T[] repeat(int n, T object, Class<T> clazz) {
+
+        T[] result = (T[]) Array.newInstance(clazz, n);
 
         for (int i = 0; i < n; i++) {
 

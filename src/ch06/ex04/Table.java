ce00a8d549d4bf081fad4c2731fa956223da2cd1
crystalDf <chendong333@gmail.com>
2017-09-27 21:52:19 +0800

    Chapter 06

--- src/ch06/ex04/Table.java
+++ src/ch06/ex04/Table.java
@@ -35,3 +35,26 @@ public class Table<K, V> {
         }
     }
 }
+
+class Entry<K, V> {
+
+    private K key;
+    private V value;
+
+    public Entry(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+}

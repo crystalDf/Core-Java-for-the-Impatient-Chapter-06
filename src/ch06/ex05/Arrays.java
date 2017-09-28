feb7d38f9985908972f4e9a20b362ab5410dd7ea
crystalDf <chendong333@gmail.com>
2017-09-28 23:30:47 +0800

    Chapter 06

---
+++ src/ch06/ex05/Arrays.java
@@ -0,0 +1,13 @@
+package ch06.ex05;
+
+public class Arrays {
+
+    public static <T> T[] swap(int i, int j, T... values) {
+
+        T temp = values[i];
+        values[i] = values[j];
+        values[j] = temp;
+
+        return values;
+    }
+}
---
+++ src/ch06/ex05/Ex05.java
@@ -0,0 +1,10 @@
+package ch06.ex05;
+
+public class Ex05 {
+
+    public static void main(String[] args) {
+
+        Double[] result = Arrays.<Double>swap(0, 1, 1.5, 2d, 3d);
+    }
+
+}
